afac137555b5cbbc829ed97d837a2388fbe84423
Master <RukiPomidory@yandex.ru>
2018-08-19 15:13:36 +0300

    Initial commit

---
+++ app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
@@ -0,0 +1,151 @@
+package co.unpredictable_procrastination.codeeditor;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.regex.*;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static String FILENAME = "sample.txt"; // имя файла
+    private EditText mEditText;
+
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        mEditText = findViewById(R.id.editText);
+        final SpannableStringBuilder text = new SpannableStringBuilder("public static void main(){\n}");
+        final ForegroundColorSpan style = new ForegroundColorSpan(Color.rgb(0, 0, 255));
+        text.setSpan(style, 0, 20, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        mEditText.setText(text);
+        mEditText.addTextChangedListener(new TextWatcher()
+        {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+//                final SpannableStringBuilder text = new SpannableStringBuilder(mEditText.getText());
+//                final ForegroundColorSpan style = new ForegroundColorSpan(Color.rgb(0, 100, 255));
+//                text.setSpan(style, 0, 20, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+//                mEditText.setText(text);
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        start + " " + before + " " + count, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_open:
+                openFile(FILENAME);
+                return true;
+            case R.id.action_save:
+                saveFile(FILENAME);
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    // Метод для открытия файла
+    private void openFile(String fileName)
+    {
+        try
+        {
+            InputStream inputStream = openFileInput(fileName);
+
+            if (inputStream != null)
+            {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null)
+                {
+                    builder.append(line).append("\n");
+                }
+
+                inputStream.close();
+                mEditText.setText(builder.toString());
+            }
+        }
+        catch (Throwable t)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Метод для сохранения файла
+    private void saveFile(String fileName)
+    {
+        try
+        {
+            OutputStream outputStream = openFileOutput(fileName, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+            osw.write(mEditText.getText().toString());
+            osw.close();
+        }
+        catch (Throwable t)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void openSettings()
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+
+    }
+}
