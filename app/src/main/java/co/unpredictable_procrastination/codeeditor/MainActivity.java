a0f7e82ba4d3b1792ddf8a56d9b10604d0b7a812
Master <RukiPomidory@yandex.ru>
2018-08-19 15:28:26 +0300

    git test

--- app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
+++ app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
