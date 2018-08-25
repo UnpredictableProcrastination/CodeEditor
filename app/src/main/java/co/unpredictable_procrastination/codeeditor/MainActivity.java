8da0b0c77a6b0bab96c0331a097fe13ea8d05dc0
Master <RukiPomidory@yandex.ru>
2018-08-25 05:04:12 +0300

    still raw, but looks like working

--- app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
+++ app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
@@ -49,19 +50,80 @@ public class MainActivity extends AppCompatActivity
 
         mainWatcher = new TextWatcher()
         {
-            Matcher matcher;
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                builder.clearSpans();
+                int keywordsCount =
+                        builder.getSpans(
+                                0,
+                                builder.length(),
+                                ForegroundColorSpan.class
+                        ).length;
+
+                int cursor = start + count;
+                updateNumBar();
+
+                int begin = start - 6;
+                int end = cursor + 6;
+                if(begin < 0)
+                {
+                    begin = 0;
+                }
+                if(end > s.length())
+                {
+                    end = s.length();
+                }
+
+                Matcher matcher = keywordPattern.matcher(s.subSequence(begin, end));
+
+                //builder.clearSpans();
                 CharSequence appending = null;
                 CharSequence replacing = null;
+                CharSequence insertion = null;
 
                 if(s.length() < builder.length())
                 {
+                    int builderEnd = cursor + 6;
+                    if(builderEnd > builder.length())
+                    {
+                        builderEnd = builder.length();
+                    }
+                    Matcher builderMatcher = keywordPattern.matcher(builder.subSequence(begin, builderEnd));
                     builder.delete(start, start + before);
+
+                    boolean found = false;
+                    while(matcher.find() && builderMatcher.find())
+                    {
+                        if(!matcher.group(0).equals(builderMatcher.group(0)))
+                        {
+                            found = true;
+                            ForegroundColorSpan[] spans =
+                                    builder.getSpans(
+                                            builderMatcher.start() + begin,
+                                            builderMatcher.end()+ begin,
+                                            ForegroundColorSpan.class
+                                    );
+                            if(spans.length > 0)
+                            {
+                                builder.removeSpan(spans[0]);
+                            }
+                        }
+                    }
+
+                    if(!found && builderMatcher.find()) {
+                        ForegroundColorSpan[] spans =
+                                builder.getSpans(
+                                        builderMatcher.start() + begin,
+                                        builderMatcher.end() + begin,
+                                        ForegroundColorSpan.class
+                                );
+                        if (spans.length > 0) {
+                            builder.removeSpan(spans[0]);
+                        }
+                    }
                 }
                 else if(builder.length() <= start)
                 {
@@ -72,6 +134,10 @@ public class MainActivity extends AppCompatActivity
                     replacing = s.subSequence(start, builder.length());
                     appending = s.subSequence(builder.length(), start + count);
 
+                }
+                else if(s.length() == builder.length() + count)
+                {
+                    insertion = s.subSequence(start, start + count);
                 }
                 else
                 {
@@ -85,48 +151,55 @@ public class MainActivity extends AppCompatActivity
                 {
                     builder.append(appending);
                 }
+                if(insertion != null)
+                {
+                    builder.insert(start, insertion);
+                }
 
-                int cursor = mainEditor.getSelectionStart();
-                updateNumBar();
-
-                matcher = keywordPattern.matcher(s);
-
+                matcher = keywordPattern.matcher(s.subSequence(begin, end));
                 boolean found = false;
 
                 while(matcher.find())
                 {
                     found = true;
 
-
-                    ForegroundColorSpan[] spans = builder.getSpans(
-                            matcher.start(),
-                            matcher.end() - 1,
-                            ForegroundColorSpan.class
-                    );
-
-                    for(int i = 0; i < spans.length; i++)
-                    {
-                        Log.i("[" + i + "]" + "colorSpan: ", spans[i].toString());
-
-//                        Log.i("Help",
-//                                Boolean.toString(colorSpan.getForegroundColor() == color) + " "
-//                                + builder.getSpans(matcher.start(), matcher.end(),ForegroundColorSpan.class).length);
-                    }
-                    Log.i("-----------------------", "__-__-__");
                     SpannableString keyword = new SpannableString(matcher.group(0));
-                    keyword.setSpan(new ForegroundColorSpan(color), 0, keyword.length(), 0);
+                    keyword.setSpan(new ForegroundColorSpan(color), 0, keyword.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-                    builder.replace(matcher.start(), matcher.end(), keyword);
+                    ForegroundColorSpan[] spans =
+                            builder.getSpans(
+                                    matcher.start() + begin,
+                                    matcher.end() + begin,
+                                    ForegroundColorSpan.class
+                            );
+                    if(spans.length > 0)
+                    {
+                        builder.removeSpan(spans[0]);
+                    }
 
+                    builder.replace(matcher.start() + begin, matcher.end() + begin, keyword);
                 }
 
-                if(found)
+                ForegroundColorSpan[] spans = builder.getSpans(
+                        0,
+                        builder.length(),
+                        ForegroundColorSpan.class
+                );
+                Log.i("length", "" + spans.length);
+                for(int i = 0; i < spans.length; i++)
+                {
+                    Log.i("[" + i + "]" + "colorSpan: ", spans[i].toString());
+                }
+                Log.i("-----------------------", "__-__-__");
+
+
+                if(found || keywordsCount != spans.length)
                 {
                     mainEditor.removeTextChangedListener(this);
-                    mainEditor.clearComposingText();
+                    //mainEditor.clearComposingText();
                     mainEditor.setText(builder, TextView.BufferType.SPANNABLE);
-                    mainEditor.setSelection(cursor);
                     mainEditor.addTextChangedListener(this);
+                    mainEditor.setSelection(cursor);
                 }
             }
 
@@ -243,15 +316,16 @@ public class MainActivity extends AppCompatActivity
                 new InputStreamReader(getResources().openRawResource(R.raw.keywords))))
         {
             String[] data = reader.readLine().split(" ");
-            pattern.append("(")
-                    .append(data[0])
-                    .append(")");
+            pattern.append("\\b(")
+                    .append(data[0]);
+                    //.append(")");
             for (int i = 1;i<data.length;i++)
             {
-                pattern.append("|(")
-                        .append(data[i])
-                        .append(")");
+                pattern.append("|")
+                        .append(data[i]);
+                        //.append(")");
             }
+            pattern.append(")\\b");
             return pattern.toString();
         }
         catch(IOException exc)
