3d1153f825a21d0f1d7771a6d378dec502eb0de2
Master <RukiPomidory@yandex.ru>
2018-08-25 20:22:28 +0300

    comments

--- app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
+++ app/src/main/java/co/unpredictable_procrastination/codeeditor/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Запоминаем количество выделенных слов до изменения текста
+                // Понадобится в конце
                 int keywordsCount =
                         builder.getSpans(
                                 0,
@@ -63,9 +65,13 @@ public class MainActivity extends AppCompatActivity
                                 ForegroundColorSpan.class
                         ).length;
 
+                // Находим позицию курсора
                 int cursor = start + count;
+
+                // Обновляем нумерацию строк
                 updateNumBar();
 
+                // Вычисляем границы диапазона, в котором будем искать слова
                 int begin = start - 6;
                 int end = cursor + 6;
                 if(begin < 0)
@@ -77,35 +83,51 @@ public class MainActivity extends AppCompatActivity
                     end = s.length();
                 }
 
-                Matcher matcher = keywordPattern.matcher(s.subSequence(begin, end));
+                Matcher matcher;
 
-                //builder.clearSpans();
+                // Объявляем строки:
+                // Эту добавим в конец
                 CharSequence appending = null;
+                // Эта пойдет на замену уже существующего отрезка
                 CharSequence replacing = null;
+                // А эту впихнем куда-нибудь
                 CharSequence insertion = null;
 
+                // Если длина строки уменьшилась - очевидно, произошло удаление
                 if(s.length() < builder.length())
                 {
+                    // Создаем matcher на этом диапазоне
+                    matcher = keywordPattern.matcher(s.subSequence(begin, end));
+
+                    // Калькулируем границы для предыдущей версии текста
                     int builderEnd = cursor + 6;
                     if(builderEnd > builder.length())
                     {
                         builderEnd = builder.length();
                     }
+
+                    // И находим в ней совпадения
                     Matcher builderMatcher = keywordPattern.matcher(builder.subSequence(begin, builderEnd));
-                    builder.delete(start, start + before);
 
                     boolean found = false;
+
+                    // Будем искать различия в найденных словах
                     while(matcher.find() && builderMatcher.find())
                     {
+                        // Нашли несоответствие
                         if(!matcher.group(0).equals(builderMatcher.group(0)))
                         {
                             found = true;
+
+                            // Получаем список стилей для данного участка
                             ForegroundColorSpan[] spans =
                                     builder.getSpans(
                                             builderMatcher.start() + begin,
                                             builderMatcher.end()+ begin,
                                             ForegroundColorSpan.class
                                     );
+
+                            // Он нам больше не нужен, сносим к чертям!
                             if(spans.length > 0)
                             {
                                 builder.removeSpan(spans[0]);
@@ -113,6 +135,10 @@ public class MainActivity extends AppCompatActivity
                         }
                     }
 
+                    // Убираем то, что пользователь удалил из текста
+                    builder.delete(start, start + before);
+
+                    // Ничего еще не нашли, но в билдере что-то осталось?
                     if(!found && builderMatcher.find()) {
                         ForegroundColorSpan[] spans =
                                 builder.getSpans(
@@ -120,6 +146,8 @@ public class MainActivity extends AppCompatActivity
                                         builderMatcher.end() + begin,
                                         ForegroundColorSpan.class
                                 );
+
+                        // Сносим к чертям!
                         if (spans.length > 0) {
                             builder.removeSpan(spans[0]);
                         }
@@ -127,22 +155,32 @@ public class MainActivity extends AppCompatActivity
                 }
                 else if(builder.length() <= start)
                 {
+                    // Длина билдера меньше позиции добавленного текста
+                    // очевидно, это нужно запихнуть в конец
                     appending = s.subSequence(start, start + count);
                 }
                 else if (builder.length() < start + count)
                 {
+                    // Билдер уже не меньше позиции нового текста, но еще не дотягивает до конца
+                    // Значит, часть перепишем, а часть добавим
                     replacing = s.subSequence(start, builder.length());
                     appending = s.subSequence(builder.length(), start + count);
 
                 }
                 else if(s.length() == builder.length() + count)
                 {
+                    // Длина билдера и нового текста вместе дают длину строки
+                    // Получается, текст куда-то впихнули
                     insertion = s.subSequence(start, start + count);
                 }
                 else
                 {
+                    // Ничего выше не подошло, но строка изменилась?
+                    // Значит, отрезок просто заменили
                     replacing = s.subSequence(start, start + count);
                 }
+
+                // Если какая-нибудь строка непустая - производим соответствующие действия
                 if(replacing != null)
                 {
                     builder.replace(start, replacing.length(), replacing);
@@ -156,35 +194,47 @@ public class MainActivity extends AppCompatActivity
                     builder.insert(start, insertion);
                 }
 
+                // Получаем новый matcher,
+                // Первый если и был, то он пал смертью храбрых во время нахождения несоответствий
                 matcher = keywordPattern.matcher(s.subSequence(begin, end));
                 boolean found = false;
 
+                // Что-то нашли?
                 while(matcher.find())
                 {
                     found = true;
 
+                    // Красим валиком в установленный цвет
                     SpannableString keyword = new SpannableString(matcher.group(0));
                     keyword.setSpan(new ForegroundColorSpan(color), 0, keyword.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+                    // Получаем список стилей в этом диапазоне
                     ForegroundColorSpan[] spans =
                             builder.getSpans(
                                     matcher.start() + begin,
                                     matcher.end() + begin,
                                     ForegroundColorSpan.class
                             );
+
+                    // На месте найденного слова что-то было?
+                    // Правильно, сносим к чертям!
                     if(spans.length > 0)
                     {
                         builder.removeSpan(spans[0]);
                     }
 
+                    // Заливаем наше свежепокрашенное слово
                     builder.replace(matcher.start() + begin, matcher.end() + begin, keyword);
                 }
 
+                // Получаем на этот раз ВСЕ стили для текста
                 ForegroundColorSpan[] spans = builder.getSpans(
                         0,
                         builder.length(),
                         ForegroundColorSpan.class
                 );
+
+                // Выводим информацию о стилях
                 Log.i("length", "" + spans.length);
                 for(int i = 0; i < spans.length; i++)
                 {
@@ -193,6 +243,9 @@ public class MainActivity extends AppCompatActivity
                 Log.i("-----------------------", "__-__-__");
 
 
+                // Если мы нашли что-нибудь новое,
+                // или количество стилей изменилось,
+                // придется залить новый текст
                 if(found || keywordsCount != spans.length)
                 {
                     mainEditor.removeTextChangedListener(this);
